df0e3bedda5bd1eafc9b6ce268e7c9aa97b0ab71
Mykola Solonyy <nicksolony@gmail.com>
2019-10-31 03:18:34 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,2 +1,11 @@
 shelf_1 = ["Mace", "Ginger", "Marjoram"]
-shelf_2 = ["Paprika"]
+shelf_2 = ["Paprika", "Fajita", "Coriander"]
+shelf_3 = ["Parsley","Sage","Rosemary"]
+
+spice_rack = [
+                shelf_1,
+                shelf_2,
+                shelf_3
+            ]
+            
+puts spice_rack
