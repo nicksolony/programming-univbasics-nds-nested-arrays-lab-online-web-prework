d1086dbd659a73dac83f928fa17daa54063156ea
Mykola Solonyy <nicksolony@gmail.com>
2019-10-31 03:29:54 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,15 @@
+shelf_1 = ["Mace", "Ginger", "Marjoram"]
+shelf_2 = ["Paprika", "Fajita", "Coriander"]
+shelf_3 = ["Parsley","Sage","Rosemary"]
+
+spice_rack = [
+                shelf_1,
+                shelf_2,
+                shelf_3
+            ]
+            
+
+
+p spice_rack[1][1]
+spice_rack [1][1]="Cumin"
+p spice_rack[1][1]
