684a60536fb4f4e4571f91953922528150c61310
Mykola Solonyy <nicksolony@gmail.com>
2019-10-31 03:24:55 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,8 +30,8 @@ def array_literal_matrix
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
   residents_sorted = [
-    ENNET_HOUSE.sorts,
-    ENFIELD_TENNIS_ACADEMY.sorts
+    ENNET_HOUSE.sort,
+    ENFIELD_TENNIS_ACADEMY.sort
     ]
 end
 
